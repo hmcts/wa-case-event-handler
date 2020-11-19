d47caeda79be6472a6b9476c0045de0e12a04e65
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-11-19 11:55:47 +0000

    Added Service to Service configuration and token generator. Added secret pull from s2s vault and copy into our wa-vault. Updated jenkins file to pull from s2s vault.

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/wacaseeventhandler/config/ServiceTokenGeneratorConfigurationTest.java
@@ -0,0 +1,23 @@
+package uk.gov.hmcts.reform.wacaseeventhandler.config;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.wacaseeventhandler.SpringBootFunctionalBaseTest;
+
+import static org.junit.Assert.assertNotNull;
+
+public class ServiceTokenGeneratorConfigurationTest extends SpringBootFunctionalBaseTest {
+
+    @Autowired
+    @Qualifier("caseEventHandlerAuthTokenGenerator")
+    private AuthTokenGenerator authTokenGenerator;
+
+    @Test
+    public void should_generate_a_token() {
+        String token = authTokenGenerator.generate();
+        assertNotNull(token);
+
+    }
+}
---
+++ src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/config/ServiceTokenGeneratorConfiguration.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.reform.wacaseeventhandler.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
+
+@Configuration
+public class ServiceTokenGeneratorConfiguration {
+
+    @Bean(name = "caseEventHandlerAuthTokenGenerator")
+    public AuthTokenGenerator authTokenGenerator(
+        @Value("${idam.s2s-auth.secret}") String secret,
+        @Value("${idam.s2s-auth.name}") String microService,
+        ServiceAuthorisationApi serviceAuthorisationApi
+    ) {
+        return AuthTokenGeneratorFactory.createDefaultGenerator(
+            secret,
+            microService,
+            serviceAuthorisationApi
+        );
+    }
+}
