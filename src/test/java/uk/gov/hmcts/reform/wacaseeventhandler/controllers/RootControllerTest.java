bac51fc7db6591fa4097a319f9a27fe882f5cc89
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-11-23 12:26:16 +0000

    Increasing test coverage

---
+++ src/test/java/uk/gov/hmcts/reform/wacaseeventhandler/controllers/RootControllerTest.java
@@ -0,0 +1,28 @@
+package uk.gov.hmcts.reform.wacaseeventhandler.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+class RootControllerTest {
+
+    private final RootController rootController = new RootController();
+
+    @Test
+    void should_return_welcome_response() {
+
+        ResponseEntity<String> responseEntity = rootController.welcome();
+
+        assertNotNull(responseEntity);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertThat(
+            responseEntity.getBody(),
+            containsString("Welcome to wa-case-event-handler")
+        );
+    }
+}
