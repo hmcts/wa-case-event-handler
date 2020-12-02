8579f5a85639037ad3c390ad134a53960b722506
david.crespo <david.crespo@hmcts.net>
2020-12-02 16:33:52 +0100

    clean up unnecessary logs

--- src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/services/InitiationTaskHandlerServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/services/InitiationTaskHandlerServiceImpl.java
@@ -1,21 +1,18 @@
 package uk.gov.hmcts.reform.wacaseeventhandler.services;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 
 @Service
-@Slf4j
 @Order(3)
 public class InitiationTaskHandlerServiceImpl implements CaseEventHandlerService {
     @Override
     public boolean canHandle() {
-        log.info("InitiationTaskHandlerServiceImpl.canHandle.false");
         return false;
     }
 
     @Override
     public void handle() {
-        log.info("InitiationTaskHandlerServiceImpl.handle");
+
     }
 }
--- src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/services/WarningHandlerServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/services/WarningHandlerServiceImpl.java
@@ -1,22 +1,18 @@
 package uk.gov.hmcts.reform.wacaseeventhandler.services;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 
 @Service
-@Slf4j
 @Order(2)
 public class WarningHandlerServiceImpl implements CaseEventHandlerService {
     @Override
     public boolean canHandle() {
-
-        log.info("WarningHandlerServiceImpl.canHandle.false");
         return false;
     }
 
     @Override
     public void handle() {
-        log.info("WarningHandlerServiceImpl.handle");
+
     }
 }
