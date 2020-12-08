84505722a1f2923fef6dab413f595037bad3c552
david.crespo <david.crespo@hmcts.net>
2020-12-08 17:29:22 +0100

    enhance functional test to check the teask is created as expected
    Add teatDown method to clean up the task

--- src/functionalTest/java/uk/gov/hmcts/reform/wacaseeventhandler/controllers/CaseEventHandlerControllerTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wacaseeventhandler/controllers/CaseEventHandlerControllerTest.java
@@ -1,17 +1,23 @@
 package uk.gov.hmcts.reform.wacaseeventhandler.controllers;
 
+import org.junit.After;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 import uk.gov.hmcts.reform.wacaseeventhandler.SpringBootFunctionalBaseTest;
 import uk.gov.hmcts.reform.wacaseeventhandler.domain.EventInformation;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 import static net.serenitybdd.rest.SerenityRest.given;
+import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static uk.gov.hmcts.reform.wacaseeventhandler.CreatorObjectMapper.asJsonString;
 
 public class CaseEventHandlerControllerTest extends SpringBootFunctionalBaseTest {
+
+    private final String caseReference = UUID.randomUUID().toString();
+    private String taskId;
 
     @Test
     public void given_validEventInformation_should_respond_with_200() {
@@ -19,7 +25,7 @@ public class CaseEventHandlerControllerTest extends SpringBootFunctionalBaseTest
         EventInformation validEventInformation = EventInformation.builder()
             .eventInstanceId("some event instance Id")
             .dateTime(LocalDateTime.now())
-            .caseReference("some case reference")
+            .caseReference(caseReference)
             .jurisdictionId("IA")
             .caseTypeId("Asylum")
             .eventId("submitAppeal")
@@ -34,5 +40,35 @@ public class CaseEventHandlerControllerTest extends SpringBootFunctionalBaseTest
             .post("/messages")
             .then()
             .statusCode(HttpStatus.NO_CONTENT.value());
+
+
+        taskId = given()
+            .header(SERVICE_AUTHORIZATION, authTokenGenerator.generate())
+            .contentType(APPLICATION_JSON_VALUE)
+            .baseUri(camundaUrl)
+            .basePath("/task")
+            .param("processVariables", "caseReference_eq_" + caseReference)
+            .when()
+            .get()
+            .prettyPeek()
+            .then()
+            .body("size()", is(1))
+            .body("[0].name", is("Process Application"))
+            .body("[0].formKey", is("processApplication"))
+            .extract()
+            .path("[0].id");
+
+    }
+
+    @After
+    public void cleanUpTask() {
+
+        given()
+            .contentType(APPLICATION_JSON_VALUE)
+            .accept(APPLICATION_JSON_VALUE)
+            .header(SERVICE_AUTHORIZATION, authTokenGenerator.generate())
+            .when()
+            .post(camundaUrl + "/task/{task-id}/complete", taskId);
+
     }
 }
--- src/testUtils/java/uk/gov/hmcts/reform/wacaseeventhandler/SpringBootFunctionalBaseTest.java
+++ src/testUtils/java/uk/gov/hmcts/reform/wacaseeventhandler/SpringBootFunctionalBaseTest.java
@@ -4,17 +4,28 @@ import io.restassured.RestAssured;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 
 @RunWith(SpringIntegrationSerenityRunner.class)
 @SpringBootTest
 @ActiveProfiles(profiles = {"local", "functional"})
 public abstract class SpringBootFunctionalBaseTest {
 
+    public static final String SERVICE_AUTHORIZATION = "ServiceAuthorization";
+
+
     @Value("${targets.instance}")
     protected String testUrl;
+
+    @Value("${targets.camunda}")
+    public String camundaUrl;
+
+    @Autowired
+    public AuthTokenGenerator authTokenGenerator;
 
     @Before
     public void setUp() {
