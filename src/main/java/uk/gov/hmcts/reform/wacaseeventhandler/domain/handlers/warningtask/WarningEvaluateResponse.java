0f73c57c5cf2aaeadf817a0e2eccf7830d65e3c6
lewis150193 <lewis.williams@hmcts.net>
2021-01-25 11:51:03 +0000

    add hasWarnings handler

--- src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/domain/handlers/warningtask/WarningEvaluateResponse.java
+++ src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/domain/handlers/warningtask/WarningEvaluateResponse.java
@@ -1,14 +1,28 @@
 package uk.gov.hmcts.reform.wacaseeventhandler.domain.handlers.warningtask;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import uk.gov.hmcts.reform.wacaseeventhandler.domain.handlers.common.DmnStringValue;
 import uk.gov.hmcts.reform.wacaseeventhandler.domain.handlers.common.EvaluateResponse;
 
 @ToString
 @EqualsAndHashCode(callSuper = true)
+@Builder
 public final class WarningEvaluateResponse extends EvaluateResponse {
 
-    // define here your DmnValues
+    private final DmnStringValue action;
 
-    // define getters and annotate constructor as per InitiateTaskEvaluateDmnResponse
+    @JsonCreator
+    public WarningEvaluateResponse(@JsonProperty("action") DmnStringValue action) {
+        super();
+        this.action = action;
+    }
+
+    public DmnStringValue getAction() {
+        return action;
+    }
+
 }
--- src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/handlers/WarningTaskHandler.java
+++ src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/handlers/WarningTaskHandler.java
@@ -2,24 +2,87 @@ package uk.gov.hmcts.reform.wacaseeventhandler.handlers;
 
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.wacaseeventhandler.clients.WorkflowApiClientToWarnTask;
+import uk.gov.hmcts.reform.wacaseeventhandler.domain.handlers.cancellationtask.CancellationCorrelationKeys;
+import uk.gov.hmcts.reform.wacaseeventhandler.domain.handlers.cancellationtask.CancellationEvaluateRequest;
+import uk.gov.hmcts.reform.wacaseeventhandler.domain.handlers.cancellationtask.CancellationEvaluateResponse;
+import uk.gov.hmcts.reform.wacaseeventhandler.domain.handlers.common.DmnStringValue;
+import uk.gov.hmcts.reform.wacaseeventhandler.domain.handlers.common.EvaluateDmnRequest;
 import uk.gov.hmcts.reform.wacaseeventhandler.domain.handlers.common.EvaluateResponse;
 import uk.gov.hmcts.reform.wacaseeventhandler.domain.handlers.common.EventInformation;
+import uk.gov.hmcts.reform.wacaseeventhandler.domain.handlers.common.ProcessVariables;
+import uk.gov.hmcts.reform.wacaseeventhandler.domain.handlers.common.SendMessageRequest;
 import uk.gov.hmcts.reform.wacaseeventhandler.domain.handlers.warningtask.WarningEvaluateResponse;
 
-import java.util.Collections;
 import java.util.List;
+
+import static uk.gov.hmcts.reform.wacaseeventhandler.services.HandlerConstants.TASK_WARN;
 
 @Service
 @Order(2)
 public class WarningTaskHandler implements CaseEventHandler {
+
+    private final WorkflowApiClientToWarnTask workflowApiClientToWarnTask;
+
+    public WarningTaskHandler(WorkflowApiClientToWarnTask workflowApiClientToWarnTask) {
+        this.workflowApiClientToWarnTask = workflowApiClientToWarnTask;
+    }
+
     @Override
     public List<WarningEvaluateResponse> evaluateDmn(EventInformation eventInformation) {
-        // placeholder for the future cancellation process
-        return Collections.emptyList();
+        String tableKey = TASK_WARN.getTableKey(
+            eventInformation.getJurisdictionId(),
+            eventInformation.getCaseTypeId()
+        );
+
+        EvaluateDmnRequest<CancellationEvaluateRequest> requestParameters = getParameterRequest(
+            eventInformation.getPreviousStateId(),
+            eventInformation.getEventId(),
+            eventInformation.getNewStateId()
+        );
+
+        return workflowApiClientToWarnTask.evaluateDmn(tableKey, requestParameters).getResults();
+    }
+
+    private EvaluateDmnRequest<CancellationEvaluateRequest> getParameterRequest(
+        String previousStateId,
+        String eventId,
+        String newStateId
+    ) {
+        CancellationEvaluateRequest variables = new CancellationEvaluateRequest(
+            new DmnStringValue(eventId),
+            new DmnStringValue(newStateId),
+            new DmnStringValue(previousStateId)
+        );
+
+        return new EvaluateDmnRequest<>(variables);
     }
 
     @Override
     public void handle(List<? extends EvaluateResponse> results, EventInformation eventInformation) {
-        // empty for now
+        results.stream()
+            .filter(result -> result instanceof CancellationEvaluateResponse)
+            .map(result -> (WarningEvaluateResponse) result)
+            .forEach(cancellationEvaluateResponse -> sendMessageToWarnTasksForGivenCorrelations(
+                eventInformation.getCaseReference(),
+                cancellationEvaluateResponse.getAction().getValue()
+            ));
+    }
+
+    private void sendMessageToWarnTasksForGivenCorrelations(String caseReference, String category) {
+        workflowApiClientToWarnTask.sendMessage(buildSendMessageRequest(category, caseReference));
+    }
+
+    private SendMessageRequest<ProcessVariables, CancellationCorrelationKeys> buildSendMessageRequest(
+        String taskCategory,
+        String caseReference
+    ) {
+        return SendMessageRequest.<ProcessVariables, CancellationCorrelationKeys>builder()
+            .messageName(TASK_WARN.getMessageName())
+            .correlationKeys(CancellationCorrelationKeys.builder()
+                                 .caseId(new DmnStringValue(caseReference))
+                                 .taskCategory(new DmnStringValue(taskCategory))
+                                 .build())
+            .build();
     }
 }
