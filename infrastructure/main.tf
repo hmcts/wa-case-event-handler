c15459e1c03a74ca9750fc7f4776adbf0dbdd219
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-01-27 09:23:36 +0000

    Added TF ASB

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -23,3 +23,47 @@ resource "azurerm_key_vault_secret" "s2s_secret_case_event_handler" {
   value        = data.azurerm_key_vault_secret.s2s_secret.value
   key_vault_id = data.azurerm_key_vault.wa_key_vault.id
 }
+
+// Azure service bus to be moved to shared infra
+
+locals {
+  topic_name        = "${var.product}-wa-case-event-topic-${var.env}"
+  subscription_name = "${var.product}-wa-case-event-subscription-${var.env}"
+  servicebus_namespace_name       = "${var.product}-servicebus-${var.env}"
+  resource_group_name             = "${var.product}-${var.env}"
+  tags = merge(
+      var.common_tags,
+      map(
+        "Team Contact", "#wa-tech",
+        "Team Name", "WA Team"
+      )
+    )
+}
+
+//Create namespace
+module "servicebus-namespace" {
+  source              = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=master"
+  name                = local.servicebus_namespace_name
+  location            = var.location
+  resource_group_name = local.resource_group_name
+  env                 = var.env
+  common_tags         = local.tags
+  sku                 = "Premium"
+}
+
+//Create topic
+module "wa-case-event-handler-topic" {
+  source                = "git@github.com:hmcts/terraform-module-servicebus-topic?ref=master"
+  name                  = local.topic_name
+  namespace_name        = local.servicebus_namespace_name
+  resource_group_name   = local.resource_group_name
+}
+
+//Create subscription
+module "wa-case-event-handler-subscription" {
+  source                = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=master"
+  name                  = local.subscription_name
+  namespace_name        = local.servicebus_namespace_name
+  resource_group_name   = local.resource_group_name
+  topic_name            = local.topic_name
+}
