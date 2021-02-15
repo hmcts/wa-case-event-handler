b00ffe0865a8b3c04925d852c7020b7fa0f68e9c
lewis150193 <lewis.williams@hmcts.net>
2021-02-15 11:15:12 +0000

    added functional test

--- src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/domain/handlers/warningtask/WarningResponse.java
+++ src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/domain/handlers/warningtask/WarningResponse.java
@@ -1,34 +1,34 @@
-package uk.gov.hmcts.reform.wacaseeventhandler.domain.handlers.warningtask;
-
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
-import uk.gov.hmcts.reform.wacaseeventhandler.domain.handlers.common.DmnStringValue;
-import uk.gov.hmcts.reform.wacaseeventhandler.domain.handlers.common.EvaluateResponse;
-
-@ToString
-@EqualsAndHashCode(callSuper = true)
-@Builder
-public final class WarningResponse extends EvaluateResponse {
-
-    private final DmnStringValue action;
-    private final DmnStringValue taskCategories;
-
-    @JsonCreator
-    public WarningResponse(@JsonProperty("action") DmnStringValue action,
-                           @JsonProperty("TaskCategories") DmnStringValue taskCategories) {
-        super();
-        this.action = action;
-        this.taskCategories = taskCategories;
-    }
-
-    public DmnStringValue getAction() {
-        return action;
-    }
-
-    public DmnStringValue getTaskCategories() {
-        return taskCategories;
-    }
-}
+//package uk.gov.hmcts.reform.wacaseeventhandler.domain.handlers.warningtask;
+//
+//import com.fasterxml.jackson.annotation.JsonCreator;
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//import lombok.Builder;
+//import lombok.EqualsAndHashCode;
+//import lombok.ToString;
+//import uk.gov.hmcts.reform.wacaseeventhandler.domain.handlers.common.DmnStringValue;
+//import uk.gov.hmcts.reform.wacaseeventhandler.domain.handlers.common.EvaluateResponse;
+//
+//@ToString
+//@EqualsAndHashCode(callSuper = true)
+//@Builder
+//public final class WarningResponse extends EvaluateResponse {
+//
+//    private final DmnStringValue action;
+//    private final DmnStringValue taskCategories;
+//
+//    @JsonCreator
+//    public WarningResponse(@JsonProperty("action") DmnStringValue action,
+//                           @JsonProperty("TaskCategories") DmnStringValue taskCategories) {
+//        super();
+//        this.action = action;
+//        this.taskCategories = taskCategories;
+//    }
+//
+//    public DmnStringValue getAction() {
+//        return action;
+//    }
+//
+//    public DmnStringValue getTaskCategories() {
+//        return taskCategories;
+//    }
+//}
