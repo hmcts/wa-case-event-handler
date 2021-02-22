182520d789b64286c918fb07653bfbc30460825c
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-02-22 11:53:30 +0000

    Fix compilation issues

--- src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/clients/CcdEventConsumer.java
+++ src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/clients/CcdEventConsumer.java
@@ -68,7 +68,7 @@ public class CcdEventConsumer implements Runnable {
                         try {
                             log.info(String.format("Processing case details: %s", loggerMsg));
 
-                            ccdEventProcessor.processMesssage(incomingMessage);
+                            ccdEventProcessor.processMessage(incomingMessage);
                             receiver.complete(message);
 
                             log.info(String.format("Processing completed successfully"
