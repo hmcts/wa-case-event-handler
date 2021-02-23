69726aa1f1422713381dc5c8949592fa47cdfc93
lewis150193 <lewis.williams@hmcts.net>
2021-02-23 16:39:41 +0000

    enabled security scan

--- security.sh
+++ security.sh
@@ -6,10 +6,8 @@ export LANG=C.UTF-8
 
 
 echo "${TEST_URL}"
-echo "${TEST_URL}"
-echo "{TEST_URL}"
-echo "${SecurityRules}"
-zap-api-scan.py -t "${TEST_URL}"/v2/api-docs -f openapi -S -d -u "${SecurityRules}" -P 1001 -l FAIL
+echo "${Rules}"
+zap-api-scan.py -t "${TEST_URL}"/v2/api-docs -f openapi -S -d -u "${Rules}" -P 1001 -l FAIL
 cat zap.out
 curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o api-report.html -f html
