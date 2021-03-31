1f028011b9fa02f47a62880967bc3aa00a88e4d5
lewis150193 <lewis.williams@hmcts.net>
2021-03-31 13:25:23 +0100

    add azure service bus toggler

--- src/functionalTest/java/uk/gov/hmcts/reform/wacaseeventhandler/config/LaunchDarklyFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wacaseeventhandler/config/LaunchDarklyFunctionalTest.java
@@ -15,31 +15,29 @@ public class LaunchDarklyFunctionalTest extends SpringBootFunctionalBaseTest {
     @Test
     public void should_hit_launch_darkly_and_return_true() {
         boolean launchDarklyFeature = launchDarklyClient.getBooleanValueFromKey("tester");
-
         assertThat(launchDarklyFeature, is(true));
     }
 
     @Test
     public void should_hit_launch_darkly_with_non_existent_key_and_return_false() {
         boolean launchDarklyFeature = launchDarklyClient.getBooleanValueFromKey("non-existent");
-
         assertThat(launchDarklyFeature, is(false));
     }
 
     @Test
-    public void should_hit_launch_darkly_with_azure_servicebus_and_return_OFF_string() {
+    public void should_hit_launch_darkly_with_azure_servicebus_and_return_off_string() {
         String launchDarklyFeature = launchDarklyClient.getStringValueFromKey("azure-servicebus");
         assertThat(launchDarklyFeature, is("OFF"));
     }
 
     @Test
-    public void should_hit_launch_darkly_with_message_servicebus_and_return_OFF_string() {
+    public void should_hit_launch_darkly_with_message_servicebus_and_return_off_string() {
         String launchDarklyFeature = launchDarklyClient.getStringValueFromKey("message-servicebus");
         assertThat(launchDarklyFeature, is("OFF"));
     }
 
     @Test
-    public void should_hit_launch_darkly_with_amqp_and_return_OFF_string() {
+    public void should_hit_launch_darkly_with_amqp_and_return_off_string() {
         String launchDarklyFeature = launchDarklyClient.getStringValueFromKey("amqp");
         assertThat(launchDarklyFeature, is("OFF"));
     }
