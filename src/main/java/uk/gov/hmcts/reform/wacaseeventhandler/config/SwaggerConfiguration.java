f5f30b373148577728d3e86c356aeaf61c5ae4d0
david.crespo <david.crespo@hmcts.net>
2021-07-13 16:29:46 +0100

    add swagger api docs

--- src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/config/SwaggerConfiguration.java
@@ -2,12 +2,12 @@ package uk.gov.hmcts.reform.wacaseeventhandler.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import uk.gov.hmcts.reform.wacaseeventhandler.Application;
 
 @Configuration
 @EnableSwagger2
@@ -18,7 +18,7 @@ public class SwaggerConfiguration {
         return new Docket(DocumentationType.SWAGGER_2)
             .useDefaultResponseMessages(false)
             .select()
-            .apis(RequestHandlerSelectors.basePackage(Application.class.getPackage().getName() + ".controllers"))
+            .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
             .paths(PathSelectors.any())
             .build();
     }
--- src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/controllers/CaseEventHandlerController.java
+++ src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/controllers/CaseEventHandlerController.java
@@ -1,17 +1,22 @@
 package uk.gov.hmcts.reform.wacaseeventhandler.controllers;
 
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.wacaseeventhandler.domain.camunda.response.EvaluateResponse;
 import uk.gov.hmcts.reform.wacaseeventhandler.domain.ccd.message.EventInformation;
 import uk.gov.hmcts.reform.wacaseeventhandler.handlers.CaseEventHandler;
 
-import java.util.List;
 import javax.validation.Valid;
+
+import java.util.List;
 
 import static org.springframework.http.ResponseEntity.noContent;
 
@@ -25,8 +30,15 @@ public class CaseEventHandlerController {
     }
 
     @ApiOperation("Handles the CCD case event message")
+    @ApiResponses({
+        @ApiResponse(
+            code = 204,
+            message = "Message processed successfully",
+            response = Object.class)
+    })
     @PostMapping(path = "/messages", consumes = {MediaType.APPLICATION_JSON_VALUE})
     @SuppressWarnings("PMD.DataflowAnomalyAnalysis")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<Void> caseEventHandler(@Valid @RequestBody EventInformation eventInformation) {
 
         for (CaseEventHandler handler : handlerServices) {
