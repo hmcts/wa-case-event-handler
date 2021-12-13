37bf1def7863b2195ceb40f2de2ae69e0ba87ced
rafalkalita <rkalitainfo@gmail.com>
2021-12-13 19:41:43 +0000

    RWA-931 Refactoring

--- src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/domain/model/CaseEventMessage.java
+++ src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/domain/model/CaseEventMessage.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.wacaseeventhandler.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import uk.gov.hmcts.reform.wacaseeventhandler.entity.MessageState;
@@ -7,128 +8,96 @@ import uk.gov.hmcts.reform.wacaseeventhandler.entity.MessageState;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+@SuppressWarnings("PMD.ExcessiveParameterList")
 public class CaseEventMessage implements Serializable {
 
     private static final long serialVersionUID = 3213665975741833471L;
 
-    @JsonProperty("MessageId")
-    private String messageId;
-    @JsonProperty("Sequence")
-    private Long sequence;
-    @JsonProperty("CaseId")
-    private String caseId;
-    @JsonProperty("EventTimestamp")
-    private LocalDateTime eventTimestamp;
-    @JsonProperty("FromDlq")
-    private Boolean fromDlq;
-    @JsonProperty("State")
-    private MessageState state;
-    @JsonProperty("MessageProperties")
-    private JsonNode messageProperties;
-    @JsonProperty("MessageContent")
-    private String messageContent;
-    @JsonProperty("Received")
-    private LocalDateTime received;
-    @JsonProperty("DeliveryCount")
-    private Integer deliveryCount;
-    @JsonProperty("HoldUntil")
-    private LocalDateTime holdUntil;
-    @JsonProperty("RetryCount")
-    private Integer retryCount;
+    private final String messageId;
+    private final Long sequence;
+    private final String caseId;
+    private final LocalDateTime eventTimestamp;
+    private final Boolean fromDlq;
+    private final MessageState state;
+    private final JsonNode messageProperties;
+    private final String messageContent;
+    private final LocalDateTime received;
+    private final Integer deliveryCount;
+    private final LocalDateTime holdUntil;
+    private final Integer retryCount;
+
+    @JsonCreator
+    public CaseEventMessage(@JsonProperty("MessageId") String messageId,
+                            @JsonProperty("Sequence") Long sequence,
+                            @JsonProperty("CaseId") String caseId,
+                            @JsonProperty("EventTimestamp") LocalDateTime eventTimestamp,
+                            @JsonProperty("FromDlq") Boolean fromDlq,
+                            @JsonProperty("State")MessageState state,
+                            @JsonProperty("MessageProperties") JsonNode messageProperties,
+                            @JsonProperty("MessageContent") String messageContent,
+                            @JsonProperty("Received") LocalDateTime received,
+                            @JsonProperty("DeliveryCount") Integer deliveryCount,
+                            @JsonProperty("HoldUntil") LocalDateTime holdUntil,
+                            @JsonProperty("RetryCount") Integer retryCount) {
+        this.messageId = messageId;
+        this.sequence = sequence;
+        this.caseId = caseId;
+        this.eventTimestamp = eventTimestamp;
+        this.fromDlq = fromDlq;
+        this.state = state;
+        this.messageProperties = messageProperties;
+        this.messageContent = messageContent;
+        this.received = received;
+        this.deliveryCount = deliveryCount;
+        this.holdUntil = holdUntil;
+        this.retryCount = retryCount;
+    }
 
     public String getMessageId() {
         return messageId;
-    }
-
-    public void setMessageId(String messageId) {
-        this.messageId = messageId;
     }
 
     public Long getSequence() {
         return sequence;
     }
 
-    public void setSequence(Long sequence) {
-        this.sequence = sequence;
-    }
-
     public String getCaseId() {
         return caseId;
-    }
-
-    public void setCaseId(String caseId) {
-        this.caseId = caseId;
     }
 
     public LocalDateTime getEventTimestamp() {
         return eventTimestamp;
     }
 
-    public void setEventTimestamp(LocalDateTime eventTimestamp) {
-        this.eventTimestamp = eventTimestamp;
-    }
-
     public Boolean getFromDlq() {
         return fromDlq;
-    }
-
-    public void setFromDlq(Boolean fromDlq) {
-        this.fromDlq = fromDlq;
     }
 
     public MessageState getState() {
         return state;
     }
 
-    public void setState(MessageState state) {
-        this.state = state;
-    }
-
     public JsonNode getMessageProperties() {
         return messageProperties;
-    }
-
-    public void setMessageProperties(JsonNode messageProperties) {
-        this.messageProperties = messageProperties;
     }
 
     public String getMessageContent() {
         return messageContent;
     }
 
-    public void setMessageContent(String messageContent) {
-        this.messageContent = messageContent;
-    }
-
     public LocalDateTime getReceived() {
         return received;
-    }
-
-    public void setReceived(LocalDateTime received) {
-        this.received = received;
     }
 
     public Integer getDeliveryCount() {
         return deliveryCount;
     }
 
-    public void setDeliveryCount(Integer deliveryCount) {
-        this.deliveryCount = deliveryCount;
-    }
-
     public LocalDateTime getHoldUntil() {
         return holdUntil;
     }
 
-    public void setHoldUntil(LocalDateTime holdUntil) {
-        this.holdUntil = holdUntil;
-    }
-
     public Integer getRetryCount() {
         return retryCount;
-    }
-
-    public void setRetryCount(Integer retryCount) {
-        this.retryCount = retryCount;
     }
 }
