0f7ea3b7f702bb79ac14ff828c215cccf4884826
Gareth Sullivan <gareth.sullivan@cgi.com>
2021-12-13 11:12:40 +0000

    RWA-999 - client connection to dead letter queue on new subscription

---
+++ src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/config/CcdCaseEventsDeadLetterQueueExecutor.java
@@ -0,0 +1,34 @@
+package uk.gov.hmcts.reform.wacaseeventhandler.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import uk.gov.hmcts.reform.wacaseeventhandler.clients.CcdCaseEventsDeadLetterQueueConsumer;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+@Configuration
+@ConditionalOnProperty("azure.servicebus.enableASB")
+public class CcdCaseEventsDeadLetterQueueExecutor {
+
+    @Value("${azure.servicebus.threads}")
+    private int concurrentSessions;
+
+    @Autowired
+    private CcdCaseEventsDeadLetterQueueConsumer serviceBusTask;
+
+    @Bean
+    @SuppressWarnings("PMD.DataflowAnomalyAnalysis")
+    public void createServiceBus() {
+        final ExecutorService executorService = Executors.newFixedThreadPool(
+            Integer.valueOf(concurrentSessions));
+
+        IntStream.range(0, concurrentSessions).forEach(
+            task -> executorService.execute(serviceBusTask));
+    }
+
+}
