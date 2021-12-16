553eb01d52116403b1669dc3235a78154a67e311
Gareth Sullivan <gareth.sullivan@cgi.com>
2021-12-16 10:11:00 +0000

    RWA-1000 - wire in eventmessagereceiver service

--- src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/services/EventMessageReceiverService.java
+++ src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/services/EventMessageReceiverService.java
@@ -47,7 +47,12 @@ public class EventMessageReceiverService {
 
     public CaseEventMessage handleDlqMessage(String messageId, String message) {
         log.info("Received Case Event Dead Letter Queue message with id '{}'", messageId);
-        return handleMessage(messageId, message, true);
+        if (featureFlagProvider.getBooleanValue(DLQ_DB_INSERT, getUserId(message))) {
+            return handleMessage(messageId, message, true);
+        } else {
+            log.info("Feature flag '{}' evaluated to false. Message not inserted into DB", DLQ_DB_INSERT.getKey());
+        }
+        return null;
     }
 
     public CaseEventMessage handleAsbMessage(String messageId, String message) {
