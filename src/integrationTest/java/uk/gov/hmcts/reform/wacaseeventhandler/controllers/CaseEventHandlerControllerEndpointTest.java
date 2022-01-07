1e1ad14c3784dd4a71351f7d5232e5f9509aa44b
Gareth Sullivan <gareth.sullivan@cgi.com>
2022-01-07 10:51:56 +0000

    RWA-1000 - Update Controller tests with launch darkly flags

--- src/integrationTest/java/uk/gov/hmcts/reform/wacaseeventhandler/controllers/CaseEventHandlerControllerEndpointTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wacaseeventhandler/controllers/CaseEventHandlerControllerEndpointTest.java
@@ -19,6 +19,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultMatcher;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.wacaseeventhandler.clients.LaunchDarklyFeatureFlagProvider;
 import uk.gov.hmcts.reform.wacaseeventhandler.clients.WorkflowApiClient;
 import uk.gov.hmcts.reform.wacaseeventhandler.domain.camunda.request.EvaluateDmnRequest;
 import uk.gov.hmcts.reform.wacaseeventhandler.domain.camunda.response.CancellationEvaluateResponse;
@@ -66,11 +67,15 @@ class CaseEventHandlerControllerEndpointTest {
 
     @MockBean
     private AuthTokenGenerator authTokenGenerator;
+
     @MockBean
     private WorkflowApiClient workflowApiClient;
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private LaunchDarklyFeatureFlagProvider launchDarklyFeatureFlagProvider;
 
     @BeforeEach
     void setUp() {
@@ -196,6 +201,7 @@ class CaseEventHandlerControllerEndpointTest {
         @Test
         void dlq_case_event_message_should_be_stored_and_return_200_ok() throws Exception {
 
+            when(launchDarklyFeatureFlagProvider.getBooleanValue(any(), any())).thenReturn(true);
             String messageId = randomMessageId();
 
             MvcResult result = postMessage(messageId, status().isCreated(), true);
@@ -224,8 +230,21 @@ class CaseEventHandlerControllerEndpointTest {
         }
 
         @Test
-        void should_store_no_caseId_unprocessable_message_and_return_200_ok() throws Exception {
+        void dlq_case_event_message_should_not_be_stored_and_return_200_ok_when_feature_flag_disabled()
+                throws Exception {
 
+            when(launchDarklyFeatureFlagProvider.getBooleanValue(any(), any())).thenReturn(false);
+            String messageId = randomMessageId();
+
+            MvcResult result = postMessage(messageId, status().isCreated(), true);
+
+            String content = result.getResponse().getContentAsString();
+            assertEquals(201, result.getResponse().getStatus(), content);
+        }
+
+        @Test
+        void should_store_no_caseId_unprocessable_message_and_return_200_ok() throws Exception {
+            when(launchDarklyFeatureFlagProvider.getBooleanValue(any(), any())).thenReturn(true);
             String messageId = randomMessageId();
             String unprocessableMessage = getCaseEventMessage(null);
 
@@ -252,7 +271,7 @@ class CaseEventHandlerControllerEndpointTest {
 
         @Test
         void should_store_parsing_error_unprocessable_message_and_return_200_ok() throws Exception {
-
+            when(launchDarklyFeatureFlagProvider.getBooleanValue(any(), any())).thenReturn(true);
             String messageId = randomMessageId();
             String unprocessableMessage = getUnprocessableCaseEventMessage();
 
