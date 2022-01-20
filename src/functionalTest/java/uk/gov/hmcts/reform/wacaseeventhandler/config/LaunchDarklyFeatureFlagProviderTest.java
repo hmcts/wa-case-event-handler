4fdf5383315b07d44780884c96d3410b57b397c3
rafalkalita <rkalitainfo@gmail.com>
2022-01-20 16:38:08 +0000

    RWA-1048 Add tests for two DLQ feature flags

--- src/functionalTest/java/uk/gov/hmcts/reform/wacaseeventhandler/config/LaunchDarklyFeatureFlagProviderTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wacaseeventhandler/config/LaunchDarklyFeatureFlagProviderTest.java
@@ -12,6 +12,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static uk.gov.hmcts.reform.wacaseeventhandler.config.features.FeatureFlag.AZURE_AMQP_LOGS;
 import static uk.gov.hmcts.reform.wacaseeventhandler.config.features.FeatureFlag.AZURE_MESSAGING_SERVICE_BUS_LOGS;
 import static uk.gov.hmcts.reform.wacaseeventhandler.config.features.FeatureFlag.AZURE_SERVICE_BUS_LOGS;
+import static uk.gov.hmcts.reform.wacaseeventhandler.config.features.FeatureFlag.DLQ_DB_INSERT;
+import static uk.gov.hmcts.reform.wacaseeventhandler.config.features.FeatureFlag.DLQ_DB_PROCESS;
 import static uk.gov.hmcts.reform.wacaseeventhandler.config.features.FeatureFlag.NON_EXISTENT_KEY;
 import static uk.gov.hmcts.reform.wacaseeventhandler.config.features.FeatureFlag.TASK_INITIATION_FEATURE;
 import static uk.gov.hmcts.reform.wacaseeventhandler.config.features.FeatureFlag.TEST_KEY;
@@ -19,6 +21,11 @@ import static uk.gov.hmcts.reform.wacaseeventhandler.config.features.FeatureFlag
 public class LaunchDarklyFeatureFlagProviderTest extends SpringBootFunctionalBaseTest {
 
     public static final String SOME_USER_ID = "some user id";
+    public static final String DB_INSERT_USER_ID_TARGET_TRUE = "some insert_true user id";
+    public static final String DB_INSERT_USER_ID_TARGET_FALSE = "some insert_false user id";
+    public static final String DB_PROCESS_USER_ID_TARGET_TRUE = "some process_true user id";
+    public static final String DB_PROCESS_USER_ID_TARGET_FALSE = "some process_false user id";
+
     @Autowired
     private LaunchDarklyFeatureFlagProvider featureFlagProvider;
 
@@ -62,5 +69,33 @@ public class LaunchDarklyFeatureFlagProviderTest extends SpringBootFunctionalBas
     public void should_hit_launch_darkly_for_task_initiation_feature_and_return_either_true_or_false() {
         boolean launchDarklyFeature = featureFlagProvider.getBooleanValue(TASK_INITIATION_FEATURE, SOME_USER_ID);
         assertThat(launchDarklyFeature, either(equalTo(true)).or(equalTo(false)));
+    }
+
+    @Test
+    public void should_hit_launch_darkly_for_wa_dlq_database_insert_when_user_id_contains_insert_true() {
+        boolean launchDarklyFeature = featureFlagProvider.getBooleanValue(DLQ_DB_INSERT,
+                                                                          DB_INSERT_USER_ID_TARGET_TRUE);
+        assertThat(launchDarklyFeature, equalTo(true));
+    }
+
+    @Test
+    public void should_hit_launch_darkly_for_wa_dlq_database_insert_when_user_id_does_not_contain_insert_true() {
+        boolean launchDarklyFeature = featureFlagProvider.getBooleanValue(DLQ_DB_INSERT,
+                                                                          DB_INSERT_USER_ID_TARGET_FALSE);
+        assertThat(launchDarklyFeature, equalTo(false));
+    }
+
+    @Test
+    public void should_hit_launch_darkly_for_wa_dlq_database_process_when_user_id_contains_process_true() {
+        boolean launchDarklyFeature = featureFlagProvider.getBooleanValue(DLQ_DB_PROCESS,
+                                                                          DB_PROCESS_USER_ID_TARGET_TRUE);
+        assertThat(launchDarklyFeature, equalTo(true));
+    }
+
+    @Test
+    public void should_hit_launch_darkly_for_wa_dlq_database_process_when_user_id_does_not_contain_process_true() {
+        boolean launchDarklyFeature = featureFlagProvider.getBooleanValue(DLQ_DB_PROCESS,
+                                                                          DB_PROCESS_USER_ID_TARGET_FALSE);
+        assertThat(launchDarklyFeature, equalTo(false));
     }
 }
