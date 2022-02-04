0a9238307f66014b4b2beca47e3ca6e2845fe860
Rafal Kalita <rkalitainfo@gmail.com>
2022-02-04 16:27:43 +0000

    RWA-1048 Add tests for two DLQ feature flags (#216)

--- src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/config/features/FeatureFlag.java
+++ src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/config/features/FeatureFlag.java
@@ -11,6 +11,7 @@ public enum FeatureFlag {
     //Features
     TASK_INITIATION_FEATURE("wa-task-initiation-feature"),
     DLQ_DB_INSERT("wa_dlq_database_insert"),
+    DLQ_DB_PROCESS("wa_dlq_database_process"),
 
     //The following keys are used for testing purposes only.
     TEST_KEY("tester"),
