927d50de3bdc14ffe05c89685c2c8a68357a97c9
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2022-03-07 15:25:45 +0000

    RWA-1197: Add integration test to cover error scenarios in ASB message consumer (for subscription & DLQ)

--- src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/config/executors/CcdMessageProcessorExecutor.java
+++ src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/config/executors/CcdMessageProcessorExecutor.java
@@ -3,16 +3,17 @@ package uk.gov.hmcts.reform.wacaseeventhandler.config.executors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.wacaseeventhandler.clients.DatabaseMessageConsumer;
 
-import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
-@Configuration
+
+@Component
 @Slf4j
 @Profile("!functional & !local")
 public class CcdMessageProcessorExecutor {
@@ -20,18 +21,37 @@ public class CcdMessageProcessorExecutor {
     @Value("${scheduledExecutors.messageProcessing.pollIntervalSeconds}")
     private int pollInterval;
 
-    @Value("${scheduledExecutors.messageProcessing.threadPoolSize}")
-    private int threadPoolSize;
-
     @Autowired
     private DatabaseMessageConsumer databaseMessageConsumer;
 
-    @Bean
-    @SuppressWarnings("PMD.DataflowAnomalyAnalysis")
-    public void createDatabaseMessageConsumer() {
-        final ScheduledExecutorService scheduledExecutorService =
-                    Executors.newScheduledThreadPool(threadPoolSize);
+    @Autowired
+    private ScheduledExecutorService databaseMessageExecutorService;
 
-        scheduledExecutorService.scheduleAtFixedRate(databaseMessageConsumer, 5, pollInterval, TimeUnit.SECONDS);
+    @PostConstruct
+    public void start() {
+        log.info("Starting Database message executor");
+        databaseMessageExecutorService.scheduleAtFixedRate(databaseMessageConsumer, 5, pollInterval, TimeUnit.SECONDS);
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        log.info("Shutting down Database message executor");
+        databaseMessageExecutorService.shutdown();
+        try {
+            // Wait a while for existing tasks to terminate
+            if (!databaseMessageExecutorService.awaitTermination(60, TimeUnit.SECONDS)) {
+                databaseMessageExecutorService.shutdownNow(); // Cancel currently executing tasks
+                // Wait a while for tasks to respond to being cancelled
+                if (!databaseMessageExecutorService.awaitTermination(60, TimeUnit.SECONDS)) {
+                    log.error("Pool did not terminate");
+                }
+            }
+        } catch (InterruptedException ie) {
+            // (Re-)Cancel if current thread also interrupted
+            databaseMessageExecutorService.shutdownNow();
+            // Preserve interrupt status
+            Thread.currentThread().interrupt();
+        }
+        log.info("Shut down Database message executor");
     }
 }
