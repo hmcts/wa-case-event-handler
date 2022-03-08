fcd669803cdc3bc5fdbaddac172da5622754e299
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2022-03-08 13:31:45 +0000

    Fix PWD issue

--- src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/util/UserIdParser.java
+++ src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/util/UserIdParser.java
@@ -18,8 +18,7 @@ public final class UserIdParser {
             JsonNode messageAsJson = new ObjectMapper().readTree(message);
             final JsonNode userIdNode = messageAsJson.findPath(USER_ID);
             if (!userIdNode.isMissingNode()) {
-                String userIdTextValue = userIdNode.textValue();
-                return userIdTextValue;
+                return userIdNode.textValue();
             }
         } catch (IllegalArgumentException | JsonProcessingException e) {
             log.info("Unable to find User Id in message");
