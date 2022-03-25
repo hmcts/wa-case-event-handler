0ed7dcf9db36eb2cecfefa3ed28ced05cc120960
rasikasri <44141067+rasikasri@users.noreply.github.com>
2022-03-25 16:01:50 +0000

    RWA-1197: Add integration test to cover error scenarios in ASB message consumer (for subscription & DLQ) (#245)
    * RWA-1197: Add integration test to cover error scenarios in ASB message consumer (for subscription & DLQ)

--- src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/config/executors/CcdEventExecutor.java
+++ src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/config/executors/CcdEventExecutor.java
@@ -1,18 +1,22 @@
 package uk.gov.hmcts.reform.wacaseeventhandler.config.executors;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.wacaseeventhandler.clients.CcdEventConsumer;
 
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
-@Configuration
+
+@Slf4j
+@Component
 @ConditionalOnProperty("azure.servicebus.enableASB")
 @Profile("!functional & !local")
 public class CcdEventExecutor {
@@ -23,14 +27,37 @@ public class CcdEventExecutor {
     @Autowired
     private CcdEventConsumer serviceBusTask;
 
-    @Bean
-    @SuppressWarnings("PMD.DataflowAnomalyAnalysis")
-    public void createServiceBus() {
-        final ExecutorService executorService = Executors.newFixedThreadPool(
-            Integer.valueOf(concurrentSessions));
+    @Autowired
+    private ExecutorService ccdEventExecutorService;
 
+    @PostConstruct
+    public void start() {
+        log.info("Starting message executor");
         IntStream.range(0, concurrentSessions).forEach(
-            task -> executorService.execute(serviceBusTask));
+            task -> ccdEventExecutorService.execute(serviceBusTask));
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        log.info("Shutting down message executor");
+        serviceBusTask.stop();
+        ccdEventExecutorService.shutdown();
+        try {
+            // Wait a while for existing tasks to terminate
+            if (!ccdEventExecutorService.awaitTermination(60, TimeUnit.SECONDS)) {
+                ccdEventExecutorService.shutdownNow(); // Cancel currently executing tasks
+                // Wait a while for tasks to respond to being cancelled
+                if (!ccdEventExecutorService.awaitTermination(60, TimeUnit.SECONDS)) {
+                    log.error("Pool did not terminate");
+                }
+            }
+        } catch (InterruptedException ie) {
+            // (Re-)Cancel if current thread also interrupted
+            ccdEventExecutorService.shutdownNow();
+            // Preserve interrupt status
+            Thread.currentThread().interrupt();
+        }
+        log.info("Shut down message executor");
     }
 
 }
