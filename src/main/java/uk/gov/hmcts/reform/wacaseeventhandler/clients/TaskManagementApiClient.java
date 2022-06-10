7e07e308c8703a84c4968a69cc24cd545b8ead29
Vamshi Gulapally <vamshi.gulapally@saivaakconsulting.com>
2022-06-10 16:45:30 +0100

    RWA-1449 perform reconfigure operation from case-event-handler

---
+++ src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/clients/TaskManagementApiClient.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.reform.wacaseeventhandler.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import uk.gov.hmcts.reform.wacaseeventhandler.domain.model.TaskOperationRequest;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@SuppressWarnings("PMD.UseObjectForClearerAPI")
+@FeignClient(
+    name = "task-management-api",
+    url = "${wa-task-management-api.url}"
+)
+public interface TaskManagementApiClient {
+
+    String SERVICE_AUTHORIZATION = "ServiceAuthorization";
+
+    @PostMapping(
+        value = "/workflow/task/operation",
+        consumes = APPLICATION_JSON_VALUE,
+        produces = APPLICATION_JSON_VALUE
+    )
+    void performOperation(@RequestHeader(SERVICE_AUTHORIZATION) String serviceAuthorisation,
+                          @RequestBody TaskOperationRequest taskOperationRequest);
+
+}
+
