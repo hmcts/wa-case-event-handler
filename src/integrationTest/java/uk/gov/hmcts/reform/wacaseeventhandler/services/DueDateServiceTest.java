d385d7145b15a0f565f390b8a3fe1f659a4ddb14
oguz akin <akinserdaroguzhan@gmail.com>
2022-07-20 19:34:00 +0100

    integration test added for dueDateService

--- src/integrationTest/java/uk/gov/hmcts/reform/wacaseeventhandler/services/DueDateServiceTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wacaseeventhandler/services/DueDateServiceTest.java
@@ -16,7 +16,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,30 +23,12 @@ class DueDateServiceTest {
 
     @Mock
     HolidayService holidayService;
-
-
+    
     private DueDateService dueDateService;
 
     @BeforeEach
     void setUp() {
         dueDateService = new DueDateService(holidayService);
-    }
-
-    @Test
-    void should_return_same_event_date_4_pm_when_working_days_allowed_is_less_than_1() {
-        ZonedDateTime eventDate =
-            ZonedDateTime.of(
-                2022, 7, 1,
-                9, 0, 0, 0,
-                ZoneId.systemDefault()
-            );
-
-        ZonedDateTime actualDate = dueDateService.calculateDueDate(eventDate, 0);
-        ZonedDateTime expectedDueDateTime = eventDate.with(
-            LocalTime.of(16, 0, 0, 0)
-        );
-        assertThat(actualDate, is(expectedDueDateTime));
-        verifyNoInteractions(holidayService);
     }
 
     @Test
@@ -75,45 +56,6 @@ class DueDateServiceTest {
     }
 
     @Test
-    void should_return_next_working_day_4_pm_when_calculated_due_date_not_matches_holiday() {
-        ZonedDateTime eventDateTime =
-            ZonedDateTime.of(
-                2022, 7, 19,
-                9, 0, 0, 0,
-                ZoneId.systemDefault()
-            );
-
-        int workingDaysAllowed = 2;
-
-        when(holidayService.isHoliday(eventDateTime.plusDays(workingDaysAllowed)))
-            .thenReturn(false);
-
-        ZonedDateTime expectedDueDate = eventDateTime.plusDays(workingDaysAllowed);
-        ZonedDateTime expectedDueDateTime = expectedDueDate.with(
-            LocalTime.of(16, 0, 0, 0)
-        );
-        ZonedDateTime actualDateTime = dueDateService.calculateDelayUntil(eventDateTime, workingDaysAllowed);
-
-        assertThat(actualDateTime, is(expectedDueDateTime));
-        verify(holidayService, times(1)).isHoliday(any());
-    }
-
-    @Test
-    void should_return_same_event_date_when_delay_duration_is_less_than_1() {
-        ZonedDateTime eventDate =
-            ZonedDateTime.of(
-                2022, 7, 1,
-                9, 0, 0, 0,
-                ZoneId.systemDefault()
-            );
-
-        ZonedDateTime actualDate = dueDateService.calculateDelayUntil(eventDate, 0);
-
-        assertThat(actualDate, is(eventDate));
-        verifyNoInteractions(holidayService);
-    }
-
-    @Test
     void should_return_next_working_day_4_pm_when_delay_date_matches_holiday() {
         ZonedDateTime eventDateTime =
             ZonedDateTime.of(
@@ -135,27 +77,6 @@ class DueDateServiceTest {
         verify(holidayService, times(2)).isHoliday(any());
     }
 
-    @Test
-    void should_return_event_date_4_pm_plus_delay_duration_when_delay_date_not_matches_holiday() {
-        ZonedDateTime eventDateTime =
-            ZonedDateTime.of(
-                2022, 7, 19,
-                9, 0, 0, 0,
-                ZoneId.systemDefault()
-            );
-
-        int delayDuration = 2;
-
-        when(holidayService.isHoliday(eventDateTime.plusDays(delayDuration)))
-            .thenReturn(false);
-
-        ZonedDateTime expectedDelayDate = eventDateTime.plusDays(delayDuration);
-        ZonedDateTime expectedDelayDateTime = expectedDelayDate.with(LocalTime.of(16, 0, 0, 0));
-        ZonedDateTime actualDateTime = dueDateService.calculateDelayUntil(eventDateTime, delayDuration);
-
-        assertThat(actualDateTime, is(expectedDelayDateTime));
-        verify(holidayService, times(1)).isHoliday(any());
-    }
 
 }
 
