1aa9b9cddf33094789be8c01f65ecd381ff2ce14
Mohammed Ali <mohammed.ali@hmcts.net>
2022-09-22 16:14:24 +0100

    RWA-1803 - validation for test

--- src/functionalTest/java/uk/gov/hmcts/reform/wacaseeventhandler/controllers/CaseEventHandlerTestingControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wacaseeventhandler/controllers/CaseEventHandlerTestingControllerFunctionalTest.java
@@ -21,13 +21,14 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static net.serenitybdd.rest.SerenityRest.given;
+import static org.awaitility.Awaitility.await;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.either;
 import static org.hamcrest.Matchers.equalToIgnoringCase;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -94,26 +95,37 @@ public class CaseEventHandlerTestingControllerFunctionalTest extends SpringBootF
             .extract()
             .path("[0].id");
 
-        Response response = findTaskDetailsForGivenTaskId(taskId);
-        String idempotencyKey = idempotencyKeyGenerator.generateIdempotencyKey(eventInformation.getEventInstanceId(),
-                                                                               "followUpNonStandardDirection");
+        await().ignoreException(AssertionError.class)
+            .pollInterval(2, SECONDS)
+            .atMost(180, SECONDS)
+            .until(
+                () -> {
+                    Response response = findTaskDetailsForGivenTaskId(taskId);
+                        String idempotencyKey = idempotencyKeyGenerator.generateIdempotencyKey(
+                            eventInformation.getEventInstanceId(),
+                            "followUpNonStandardDirection"
+                        );
 
-        response.then().assertThat()
-            .statusCode(HttpStatus.OK.value())
-            .and().contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body("caseTypeId.value", equalToIgnoringCase("asylum"))
-            .body("jurisdiction.value", equalToIgnoringCase("ia"))
-            .body("idempotencyKey.value", is(idempotencyKey))
-            .body("dueDate.value", notNullValue())
-            .body("taskState.value", either(equalToIgnoringCase("unconfigured")).or(equalToIgnoringCase("unassigned")))
-            .body("hasWarnings.value", is(false))
-            .body("caseId.value", is(caseIdForTask))
-            .body("name.value", equalToIgnoringCase("Follow-up non-standard direction"))
-            .body("workingDaysAllowed.value", is(2))
-            .body("isDuplicate.value", is(false))
-            .body("delayUntil.value", CoreMatchers.notNullValue())
-            .body("taskId.value", equalToIgnoringCase("followUpNonStandardDirection"))
-            .body("warningList.value", is("[]"));
+                        response.then().assertThat()
+                            .statusCode(HttpStatus.OK.value())
+                            .and().contentType(MediaType.APPLICATION_JSON_VALUE)
+                            .body("caseTypeId.value", equalToIgnoringCase("asylum"))
+                            .body("jurisdiction.value", equalToIgnoringCase("ia"))
+                            .body("idempotencyKey.value", is(idempotencyKey))
+                            .body("dueDate.value", notNullValue())
+                            .body("taskState.value", equalToIgnoringCase("unassigned"))
+                            .body("hasWarnings.value", is(false))
+                            .body("caseId.value", is(caseIdForTask))
+                            .body("name.value", equalToIgnoringCase("Follow-up non-standard direction"))
+                            .body("workingDaysAllowed.value", is(2))
+                            .body("isDuplicate.value", is(false))
+                            .body("delayUntil.value", CoreMatchers.notNullValue())
+                            .body("taskId.value", equalToIgnoringCase("followUpNonStandardDirection"))
+                            .body("warningList.value", is("[]"));
+                        return true;
+                });
+
+
     }
 
     @Test
