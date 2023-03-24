40446723c6a5a599a13e66170f4013489c8bd579
dharmendra kumar <dharmendra.kumar@hmcts.net>
2023-03-24 09:40:15 +0000

    resolve cache issue

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/wacaseeventhandler/services/calendar/CalendarCacheManagerTest.java
@@ -0,0 +1,94 @@
+package uk.gov.hmcts.reform.wacaseeventhandler.services.calendar;
+
+import com.github.benmanes.caffeine.cache.Ticker;
+import com.google.common.testing.FakeTicker;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.wacaseeventhandler.Application;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static uk.gov.hmcts.reform.wacaseeventhandler.services.calendar.DelayUntilCalculator.DATE_FORMATTER;
+import static uk.gov.hmcts.reform.wacaseeventhandler.services.calendar.DelayUntilCalculator.DEFAULT_NON_WORKING_CALENDAR;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = CalendarCacheManagerTest.TestConfiguration.class)
+@ActiveProfiles({"integration"})
+public class CalendarCacheManagerTest {
+
+    @Autowired
+    private DelayUntilConfigurator delayUntilConfigurator;
+
+    @SpyBean
+    private PublicHolidayService publicHolidayService;
+
+    @DisplayName("(Access calendars successfully and retrieve results which are also cached)")
+    @Test
+    public void shouldCacheThePublicHolidayCalendarResponse() {
+        String givenDelayUntilOrigin = LocalDate.of(2022, 12, 26).format(DATE_FORMATTER);
+
+        DelayUntilObject delayUntilObject = DelayUntilObject.builder()
+            .delayUntilOrigin(givenDelayUntilOrigin + "T00:30")
+            .delayUntilIntervalDays(4)
+            .delayUntilNonWorkingCalendar(DEFAULT_NON_WORKING_CALENDAR)
+            .delayUntilNonWorkingDaysOfWeek("SATURDAY,SUNDAY")
+            .delayUntilSkipNonWorkingDays(true)
+            .delayUntilMustBeWorkingDay("Next")
+            .delayUntilTime(null)
+            .build();
+
+        LocalDateTime localDateTime = delayUntilConfigurator.calculateDelayUntil(delayUntilObject);
+        assertThat(localDateTime).isEqualTo("2023-01-03T00:30");
+        verify(publicHolidayService, times(1)).getPublicHolidays(DEFAULT_NON_WORKING_CALENDAR);
+    }
+
+    @DisplayName("(Access calendars successfully if cached information is not available)")
+    @Test
+    public void shouldCallHolidayServiceIfCashExpires() {
+        String givenDelayUntilOrigin = LocalDate.of(2022, 12, 26).format(DATE_FORMATTER);
+
+        TestConfiguration.fakeTicker.advance(25, TimeUnit.HOURS);
+
+        DelayUntilObject delayUntilObject = DelayUntilObject.builder()
+            .delayUntilOrigin(givenDelayUntilOrigin + "T00:30")
+            .delayUntilIntervalDays(4)
+            .delayUntilNonWorkingCalendar(DEFAULT_NON_WORKING_CALENDAR)
+            .delayUntilNonWorkingDaysOfWeek("SATURDAY,SUNDAY")
+            .delayUntilSkipNonWorkingDays(true)
+            .delayUntilMustBeWorkingDay("Next")
+            .delayUntilTime(null)
+            .build();
+
+        LocalDateTime localDateTime = delayUntilConfigurator.calculateDelayUntil(delayUntilObject);
+        assertThat(localDateTime).isEqualTo("2023-01-03T00:30");
+        verify(publicHolidayService, times(1)).getPublicHolidays(DEFAULT_NON_WORKING_CALENDAR);
+    }
+
+    @Configuration
+    @Import(Application.class)
+    public static class TestConfiguration {
+
+        static FakeTicker fakeTicker = new FakeTicker();
+
+        @Bean
+        public Ticker ticker() {
+            return fakeTicker::read;
+        }
+
+    }
+
+}
