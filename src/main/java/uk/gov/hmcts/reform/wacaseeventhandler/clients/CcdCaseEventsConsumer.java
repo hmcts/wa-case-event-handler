9c3d33dd6ab6cf13e696f7a73621287c2c641e8e
rafalkalita <rkalitainfo@gmail.com>
2023-06-15 11:43:00 +0100

    RWA-2607 Handle null and ServiceBusException

--- src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/clients/CcdCaseEventsConsumer.java
+++ src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/clients/CcdCaseEventsConsumer.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.wacaseeventhandler.clients;
 
+import com.azure.messaging.servicebus.ServiceBusException;
 import com.azure.messaging.servicebus.ServiceBusReceiverClient;
 import com.azure.messaging.servicebus.ServiceBusSessionReceiverClient;
 import lombok.extern.slf4j.Slf4j;
@@ -42,26 +43,38 @@ public class CcdCaseEventsConsumer implements Runnable {
     @SuppressWarnings({"PMD.DataflowAnomalyAnalysis"})
     protected void consumeMessage(ServiceBusSessionReceiverClient sessionReceiver) {
         try (ServiceBusReceiverClient receiver = sessionReceiver.acceptNextSession()) {
-            receiver.receiveMessages(1).forEach(
-                message -> {
-                    try {
-                        String messageId = message.getMessageId();
-                        String sessionId = message.getSessionId();
-                        log.info("Received CCD Case Event message with id '{}' and case id '{}'", messageId, sessionId);
+            if (receiver != null) {
+                receiver.receiveMessages(1).forEach(
+                    message -> {
+                        try {
+                            String messageId = message.getMessageId();
+                            String sessionId = message.getSessionId();
+                            log.info(
+                                "Received CCD Case Event message with id '{}' and case id '{}'",
+                                messageId,
+                                sessionId
+                            );
 
-                        eventMessageReceiverService.handleCcdCaseEventAsbMessage(messageId, sessionId,
-                                new String(message.getBody().toBytes()));
-                        receiver.complete(message);
+                            eventMessageReceiverService
+                                .handleCcdCaseEventAsbMessage(messageId, sessionId,
+                                                              new String(message.getBody().toBytes()));
+                            receiver.complete(message);
 
-                        log.info("CCD Case Event message with id '{}' handled successfully", messageId);
-                    } catch (Exception ex) {
-                        log.error("Error processing CCD Case Event message with id '{}' - "
-                                + "abandon the processing and ASB will re-deliver it", message.getMessageId());
-                        receiver.abandon(message);
-                    }
-                });
+                            log.info("CCD Case Event message with id '{}' handled successfully", messageId);
+                        } catch (Exception ex) {
+                            log.error(
+                                "Error processing CCD Case Event message with id '{}' - "
+                                    + "abandon the processing and ASB will re-deliver it",
+                                message.getMessageId()
+                            );
+                            receiver.abandon(message);
+                        }
+                    });
+            }
         } catch (IllegalStateException ex) {
             log.info("Timeout: No CCD Case Event messages received waiting for next session.");
+        } catch (ServiceBusException ex) {
+            log.error("Error occurred while receiving messages: {}", ex.getMessage());
         } catch (Exception ex) {
             log.error("Error occurred while closing the session", ex);
         }
