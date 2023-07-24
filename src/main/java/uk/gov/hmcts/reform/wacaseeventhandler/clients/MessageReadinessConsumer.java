07166f5aff360f8388096bd6d7715b9ade1d3464
Ravi Aghera <Ravi.Aghera@HMCTS.net>
2023-07-24 14:13:55 +0100

    Quick wins for reducing logs

--- src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/clients/MessageReadinessConsumer.java
+++ src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/clients/MessageReadinessConsumer.java
@@ -45,12 +45,12 @@ public class MessageReadinessConsumer implements Runnable {
     @Override
     @Transactional
     public void run() {
-        log.info("Running message readiness check");
+        log.debug("Running message readiness check");
         try {
             final List<CaseEventMessageEntity> allMessageInNewState =
                 caseEventMessageRepository.getAllMessagesInNewState();
 
-            log.info("Number of messages to check the readiness {}", allMessageInNewState.size());
+            log.debug("Number of messages to check the readiness {}", allMessageInNewState.size());
 
             allMessageInNewState.forEach(this::checkMessageToMoveToReadyState);
 
