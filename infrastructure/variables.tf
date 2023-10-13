29d346350340e06cf343d6408943da3573a3963e
Ravi Aghera <Ravi.Aghera@HMCTS.net>
2023-10-13 16:43:39 +0100

    infra for flex server

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -33,3 +33,9 @@ variable "postgresql_database_name" {
 variable "postgresql_user" {
   default = "wa_wa"
 }
+
+variable "jenkins_AAD_objectId" {
+  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+}
+
+variable "aks_subscription_id" {} # provided by the Jenkins library, ADO users will need to specify this
