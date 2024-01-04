9c59e66be248de314ec6959f3086538a9084e3db
vasu <vasu.kaveri@hmcts.net>
2024-01-04 14:41:48 +0000

    RWA-3047: sonar issue fix

--- src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/config/executors/MessageReadinessExecutor.java
+++ src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/config/executors/MessageReadinessExecutor.java
@@ -33,7 +33,7 @@ public class MessageReadinessExecutor {
     public void start() {
         log.info("Starting message readiness executor");
         try {
-            messageReadinessExecutorService.scheduleWithFixedDelay(messageReadinessConsumer, 10000, pollInterval,
+            messageReadinessExecutorService.scheduleWithFixedDelay(messageReadinessConsumer, 10_000, pollInterval,
                                                                    TimeUnit.MILLISECONDS);
         } catch (Exception ex) {
             log.error("Error while starting readiness executor", ex);
