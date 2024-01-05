8895154a520391edf619e842d01a2e7998b8b0d6
vasu <vasu.kaveri@hmcts.net>
2024-01-05 14:51:42 +0000

    RWA-3047: retry

--- src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/config/executors/MessageReadinessExecutor.java
+++ src/main/java/uk/gov/hmcts/reform/wacaseeventhandler/config/executors/MessageReadinessExecutor.java
@@ -33,7 +33,7 @@ public class MessageReadinessExecutor {
     public void start() {
         log.info("Starting message readiness executor");
         try {
-            messageReadinessExecutorService.scheduleWithFixedDelay(messageReadinessConsumer, 15_000, pollInterval,
+            messageReadinessExecutorService.scheduleWithFixedDelay(messageReadinessConsumer, 9000, pollInterval,
                                                                    TimeUnit.MILLISECONDS);
             log.info("Readiness check thread started successfully");
         } catch (Exception ex) {
