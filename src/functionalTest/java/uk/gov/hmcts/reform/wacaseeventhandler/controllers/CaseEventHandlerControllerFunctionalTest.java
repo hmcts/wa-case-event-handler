66608c311dfce9b5175e65e3ca5a9770425d683a
Aditya Dwadasi <aditya.dwadasi@hmcts.net>
2026-07-01 14:44:41 +0100

    Checkstyle issues

--- src/functionalTest/java/uk/gov/hmcts/reform/wacaseeventhandler/controllers/CaseEventHandlerControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wacaseeventhandler/controllers/CaseEventHandlerControllerFunctionalTest.java
@@ -1264,23 +1264,25 @@ public class CaseEventHandlerControllerFunctionalTest extends MessagingTests {
         log.info(String.format("Completing task : %s", taskId));
 
         await().untilAsserted(() -> {
-                Response response = given()
-                    .header(SERVICE_AUTHORIZATION, s2sToken)
-                    .accept(APPLICATION_JSON_VALUE)
-                    .contentType(APPLICATION_JSON_VALUE)
-                    .body(body)
-                    .when()
-                    .post(camundaUrl + "/task/{task-id}/complete", taskId);
+            Response response = given()
+                .header(SERVICE_AUTHORIZATION, s2sToken)
+                .accept(APPLICATION_JSON_VALUE)
+                .contentType(APPLICATION_JSON_VALUE)
+                .body(body)
+                .when()
+                .post(camundaUrl + "/task/{task-id}/complete", taskId);
 
-                assertTrue(
-                    HttpStatus.valueOf(response.getStatusCode()).is2xxSuccessful(),
-                    String.format("Unexpected response status: %d (%s).Headers: %s Body: %s",
-                                  response.getStatusCode(),
-                                  HttpStatus.valueOf(response.getStatusCode()).name(),
-                                  response.getHeaders().toString(),
-                                  response.getBody().asString())
-                );
-            });
+            assertTrue(
+                HttpStatus.valueOf(response.getStatusCode()).is2xxSuccessful(),
+                String.format(
+                    "Unexpected response status: %d (%s).Headers: %s Body: %s",
+                    response.getStatusCode(),
+                    HttpStatus.valueOf(response.getStatusCode()).name(),
+                    response.getHeaders().toString(),
+                    response.getBody().asString()
+                )
+            );
+        });
 
         await().untilAsserted(() -> assertTaskDeleteReason(taskId, status));
     }
